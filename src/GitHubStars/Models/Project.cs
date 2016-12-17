f2a0a96db575ab2745efb9e20cca31d6f9e1076d
ian Wilcox <ian Wilcox>
2016-12-17 15:35:00 -0800

    layout files for model and project. can call api but cant return - still a little buggy

--- src/GitHubStars/Models/Project.cs
+++ src/GitHubStars/Models/Project.cs
@@ -1,11 +1,51 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using RestSharp.Deserializers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 
 namespace GitHubStars.Models
 {
     public class Project
     {
+        public string name { get; set; }
+        public string stars { get; set; }
+        public static Dictionary<string, string> GetProjects()
+        {
+            var client = new RestClient("https://api.github.com/users/iantwilcox91/repos");
+            var request = new RestRequest("", Method.GET);
+            request.AddParameter("access_token", "f6d2a0d542f6b213fb843baf5078d5edc85736ea");
+            request.AddHeader("User", "iantwilcox91");
+            request.AddHeader("Accept", "application/vnd.github.v3+json");
+
+            var response = new RestResponse();
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+
+            JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(response.Content);
+            var MyProjects = JsonConvert.DeserializeObject<List<Project>>(jsonResponse.ToString());
+            Dictionary<string, string> model = new Dictionary<string, string>();
+            foreach (var project in MyProjects)
+            {
+                model.Add(project.name, project.stars);
+            }
+            return model;
+
+        }
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response =>
+            {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
+        }
     }
 }
