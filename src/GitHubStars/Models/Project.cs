cb01b90e8247fc4bd51727ef7bc75341da2580c8
ian Wilcox <ian Wilcox>
2016-12-17 17:53:24 -0800

    can return projects!

--- src/GitHubStars/Models/Project.cs
+++ src/GitHubStars/Models/Project.cs
@@ -13,13 +13,15 @@ namespace GitHubStars.Models
     public class Project
     {
         public string name { get; set; }
-        public string stars { get; set; }
+        public string stargazers_count { get; set; }
         public static List<Project> GetProjects()
         {
             var client = new RestClient("https://api.github.com/users/iantwilcox91/repos");
-            var request = new RestRequest("", Method.GET);
+            var request = new RestRequest(Method.GET);
+            //var request = new RestRequest("???maybe i have to tell it a file or something here??" , Method.GET);
+
             request.AddParameter("access_token", "f9715624905ca91313383c9d9bec097674c2c3f8");
-            request.AddHeader("User", "iantwilcox91");
+            request.AddHeader("User-Agent", "iantwilcox91");
             request.AddHeader("Accept", "application/vnd.github.v3+json");
 
             var response = new RestResponse();
@@ -28,8 +30,9 @@ namespace GitHubStars.Models
                 response = await GetResponseContentAsync(client, request) as RestResponse;
             }).Wait();
 
-            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
-            var projectList = JsonConvert.DeserializeObject<List<Project>>(jsonResponse["projects"].ToString());
+            JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(response.Content);
+            //Unable to cast object of type 'Newtonsoft.Json.Linq.JArray' to type 'Newtonsoft.Json.Linq.JObject'. -- using JArray
+            var projectList = JsonConvert.DeserializeObject<List<Project>>(jsonResponse.ToString());
             return projectList;
         }
         public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
